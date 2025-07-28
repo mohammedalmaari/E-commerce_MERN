8eacf156a80decb1c0020d858806ded29aeb560b
mohammedalmaari <145218003+mohammedalmaari@users.noreply.github.com>
2025-07-28 12:54:35 +0300

    scafolding the frontend projects + update folder strurcture

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500 text-center'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+      <div className='bg-green-500'>jbjkjk</div>
+    </>
+  )
+}
+
+export default App
